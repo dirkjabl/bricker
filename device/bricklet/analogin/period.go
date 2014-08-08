230ad82d71d1dd8a1a928316a2cf2b1bc0fe84b6
dirkjabl <dirk@jabl.net>
2014-08-08 12:00:07 +0200

    Analog In Bricklet subscriber use now device generator.

--- device/bricklet/analogin/period.go
+++ device/bricklet/analogin/period.go
@@ -7,23 +7,19 @@ package analogin
 import (
 	"github.com/dirkjabl/bricker"
 	"github.com/dirkjabl/bricker/device"
-	"github.com/dirkjabl/bricker/net/packet"
-	"github.com/dirkjabl/bricker/subscription"
-	"github.com/dirkjabl/bricker/util/hash"
 )
 
 // SetVoltageCallbackPeriod creates the subscriber to set the callback period.
 // Default value is 0. A value of 0 deactivates the periodical callbacks.
 // VoltagePeriod is only triggered if the voltage has changed since the last triggering.
 func SetVoltageCallbackPeriod(id string, uid uint32, pe *device.Period, handler func(device.Resulter, error)) *device.Device {
-	fid := function_set_voltage_callback_period
-	svcp := device.New(device.FallbackId(id, "SetVoltageCallbackPeriod"))
-	p := packet.NewSimpleHeaderPayload(uid, fid, true, pe)
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, p, false)
-	svcp.SetSubscription(sub)
-	svcp.SetResult(&device.EmptyResult{})
-	svcp.SetHandler(handler)
-	return svcp
+	return device.Generator{
+		Id:         device.FallbackId(id, "SetVoltageCallbackPeriod"),
+		Fid:        function_set_voltage_callback_period,
+		Uid:        uid,
+		Data:       pe,
+		Handler:    handler,
+		WithPacket: true}.CreateDevice()
 }
 
 // SetVoltageCallbackPeriodFuture is a future pattern version for a synchronized call of the subscriber.
@@ -44,14 +40,13 @@ func SetVoltageCallbackPeriodFuture(brick *bricker.Bricker, connectorname string
 
 // GetVoltageCallbackPeriod creates a subsctiber to get the callback period value.
 func GetVoltageCallbackPeriod(id string, uid uint32, handler func(device.Resulter, error)) *device.Device {
-	fid := function_get_voltage_callback_period
-	gvcp := device.New(device.FallbackId(id, "GetVoltageCallbackPeriod"))
-	p := packet.NewSimpleHeaderOnly(uid, fid, true)
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, p, false)
-	gvcp.SetSubscription(sub)
-	gvcp.SetResult(&device.Period{})
-	gvcp.SetHandler(handler)
-	return gvcp
+	return device.Generator{
+		Id:         device.FallbackId(id, "GetVoltageCallbackPeriod"),
+		Fid:        function_get_voltage_callback_period,
+		Uid:        uid,
+		Result:     &device.Period{},
+		Handler:    handler,
+		WithPacket: true}.CreateDevice()
 }
 
 // GetVoltageCallbackPeriodFuture is a future pattern version for a synchronized call of the subsctiber.
@@ -80,14 +75,13 @@ func GetVoltageCallbackPeriodFuture(brick *bricker.Bricker, connectorname string
 // Default value is 0. A value of 0 deactivates the periodical callbacks.
 // AnalogValuePeriod is only triggered if the voltage has changed since the last triggering.
 func SetAnalogValueCallbackPeriod(id string, uid uint32, pe *device.Period, handler func(device.Resulter, error)) *device.Device {
-	fid := function_set_analog_value_callback_period
-	savcp := device.New(device.FallbackId(id, "SetAnalogValueCallbackPeriod"))
-	p := packet.NewSimpleHeaderPayload(uid, fid, true, pe)
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, p, false)
-	savcp.SetSubscription(sub)
-	savcp.SetResult(&device.EmptyResult{})
-	savcp.SetHandler(handler)
-	return savcp
+	return device.Generator{
+		Id:         device.FallbackId(id, "SetAnalogValueCallbackPeriod"),
+		Fid:        function_set_analog_value_callback_period,
+		Uid:        uid,
+		Data:       pe,
+		Handler:    handler,
+		WithPacket: true}.CreateDevice()
 }
 
 // SetAnalogValueCallbackPeriodFuture is a future pattern version for a synchronized call of the subscriber.
@@ -108,14 +102,13 @@ func SetAnalogValueCallbackPeriodFuture(brick *bricker.Bricker, connectorname st
 
 // GetAnalogValueCallbackPeriod creates a subsctiber to get the callback period value.
 func GetAnalogValueCallbackPeriod(id string, uid uint32, handler func(device.Resulter, error)) *device.Device {
-	fid := function_get_analog_value_callback_period
-	gavcp := device.New(device.FallbackId(id, "GetAnalogValueCallbackPeriod"))
-	p := packet.NewSimpleHeaderOnly(uid, fid, true)
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, p, false)
-	gavcp.SetSubscription(sub)
-	gavcp.SetResult(&device.Period{})
-	gavcp.SetHandler(handler)
-	return gavcp
+	return device.Generator{
+		Id:         device.FallbackId(id, "GetAnalogValueCallbackPeriod"),
+		Fid:        function_get_analog_value_callback_period,
+		Uid:        uid,
+		Result:     &device.Period{},
+		Handler:    handler,
+		WithPacket: true}.CreateDevice()
 }
 
 // GetAnalogValueCallbackPeriodFuture is a future pattern version for a synchronized call of the subsctiber.
@@ -143,23 +136,25 @@ func GetAnalogValueCallbackPeriodFuture(brick *bricker.Bricker, connectorname st
 // VoltagePeriod creates a subscriber for the periodical voltage callback.
 // Is only triggered if the voltage changed, since last triggering.
 func VoltagePeriod(id string, uid uint32, handler func(device.Resulter, error)) *device.Device {
-	fid := callback_voltage
-	vp := device.New(device.FallbackId(id, "VoltagePeriod"))
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, nil, true)
-	vp.SetSubscription(sub)
-	vp.SetResult(&Voltage{})
-	vp.SetHandler(handler)
-	return vp
+	return device.Generator{
+		Id:         device.FallbackId(id, "VoltagePeriod"),
+		Fid:        callback_voltage,
+		Uid:        uid,
+		Result:     &Voltage{},
+		Handler:    handler,
+		IsCallback: true,
+		WithPacket: false}.CreateDevice()
 }
 
 // AnalogValuePeriod creates a subscriber for the periodical analog value callback.
 // Is only triggered if the value changed, since last triggering.
 func AnalogValuePeriod(id string, uid uint32, handler func(device.Resulter, error)) *device.Device {
-	fid := callback_analog_value
-	avp := device.New(device.FallbackId(id, "AnalogValuePeriod"))
-	sub := subscription.New(hash.ChoosenFunctionIDUid, uid, fid, nil, true)
-	avp.SetSubscription(sub)
-	avp.SetResult(&AnalogValue{})
-	avp.SetHandler(handler)
-	return avp
+	return device.Generator{
+		Id:         device.FallbackId(id, "AnalogValuePeriod"),
+		Fid:        callback_analog_value,
+		Uid:        uid,
+		Result:     &AnalogValue{},
+		Handler:    handler,
+		IsCallback: true,
+		WithPacket: false}.CreateDevice()
 }
