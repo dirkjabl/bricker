37918c4d49951561be028fdb768e9b2b135c3d4f
dirkjabl <dirk@jabl.net>
2014-08-14 16:57:50 +0200

    Start support for the Motion Detector Bricklet.

---
+++ device/bricklet/motiondetector/motiondetector.go
@@ -0,0 +1,12 @@
+// Copyright 2014 Dirk Jablonowski. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Collection of subscriber for the Motion Detector Bricklet.
+package motiondetector
+
+const (
+	function_get_motion_detected   = uint8(1)
+	callback_motion_detected       = uint8(2)
+	callback_detection_cycle_ended = uint8(3)
+)
